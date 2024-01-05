9e35409bdfd40f15b65c6196b89c995f7c83478b
Igor <107538706+igor-stanimirovic@users.noreply.github.com>
2024-01-05 10:55:05 +0100

    Added DoubleJump feature

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
     [SerializeField] float jumpForce = 7;
     [SerializeField] float movementSpeed = 4;
+    [SerializeField] LayerMask ground;
     Rigidbody2D rb;
+    BoxCollider2D boxColl;
     Animator anim;
     SpriteRenderer spriteRend;
     float directionX;
-    enum MovementState { idle, running, jumping, falling};
+    bool doubleJump;
+    enum MovementState {idle, running, jumping, falling};
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        boxColl = GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
         spriteRend = GetComponent<SpriteRenderer>();
     }
@@ -23,24 +29,34 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        directionX = Input.GetAxis("Horizontal");
 
+        directionX = Input.GetAxis("Horizontal");
         transform.position += transform.right * directionX * movementSpeed * Time.deltaTime;
+
+        // Jumping and double jumping conditions
+        if (!Input.GetButtonDown("Jump") && IsOnGround())
+        {
+            doubleJump = false;
+        }
 
         if (Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(new Vector2(0,jumpForce),ForceMode2D.Impulse);
+            if (IsOnGround() || doubleJump)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                doubleJump = !doubleJump;
+            }
         }
 
         UpdateAnimationStatus();
-        
 
     }
+
+
     // Animation runing & idle switcher method
     void UpdateAnimationStatus()
     {
         MovementState state;
-
 
         // Running animation contoller
         if (directionX > 0)
@@ -69,6 +85,12 @@ public class PlayerMovement : MonoBehaviour
             state = MovementState.falling;
         }
         anim.SetInteger("state",(int)state);
+    }
+
+
+    bool IsOnGround()
+    {
+        return Physics2D.BoxCast(boxColl.bounds.center, boxColl.bounds.size, 0, Vector2.down, 0.1f, ground);
     }
 
 }
